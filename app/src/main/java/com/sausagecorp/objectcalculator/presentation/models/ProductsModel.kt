85b3ea17f0e179318d060c4899e02a39165840da
maks0n4ikus <105717673+maks0n4ikus@users.noreply.github.com>
2023-01-17 15:26:35 +0400

    Still making main screen layout

---
+++ app/src/main/java/com/sausagecorp/objectcalculator/presentation/models/ProductsModel.kt
@@ -0,0 +1,8 @@
+package com.sausagecorp.objectcalculator.presentation.models
+
+data class ProductsModel(
+    val categoryName: String,
+    val productName: String,
+    val productPrice: Int,
+    val productsCount: Int
+)
--- app/src/main/java/com/sausagecorp/objectcalculator/presentation/screens/main/MainFragment.kt
+++ app/src/main/java/com/sausagecorp/objectcalculator/presentation/screens/main/MainFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sausagecorp.objectcalculator.databinding.FragmentMainBinding
+import com.sausagecorp.objectcalculator.presentation.adapters.ProductsAdapter
+import com.sausagecorp.objectcalculator.presentation.models.ProductsModel
 
 class MainFragment : Fragment() {
 
@@ -21,6 +24,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initCountButton()
+        initProductsRv()
         viewModel.objectVolume.observe(viewLifecycleOwner) {
             binding.volumeTextView.text = it.toString()
         }
@@ -33,6 +37,20 @@ class MainFragment : Fragment() {
             binding.objectWidthEditText.text.toString().toDouble(),
             binding.objectTopHeightEditText.text.toString().toDouble()
         )
+    }
+
+    private fun initProductsRv() {
+        val productsList: ArrayList<ProductsModel> = arrayListOf(
+            ProductsModel("Пол", "Линолеум 5х3", 1200, 4),
+            ProductsModel("Потолок", "Натяжной потолок", 900, 6),
+            ProductsModel("Стены", "Жесткая краска для стен", 1700, 12),
+        )
+        val adapterProducts = ProductsAdapter(productsList)
+        val rv = binding.productsRv
+        rv.apply {
+            adapter = adapterProducts
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
 }
