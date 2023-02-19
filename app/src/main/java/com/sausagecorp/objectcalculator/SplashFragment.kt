fcc89bf4083ae1ca977b045b65171596fdda4ee0
ilya <solomon-2021@list.ru>
2023-02-19 16:13:17 +0400

    Added icon, manifest fix

---
+++ app/src/main/java/com/sausagecorp/objectcalculator/SplashFragment.kt
@@ -0,0 +1,20 @@
+package com.sausagecorp.objectcalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+    }
+}
